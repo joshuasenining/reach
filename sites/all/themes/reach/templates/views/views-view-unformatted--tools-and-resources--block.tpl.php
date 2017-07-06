08fc9ecccdc122e08468a4a254c6e7f79970cf73
Drupal-Dev <drupaldev@mckinsey.com>
2017-07-06 01:21:36 -0400

    changes from dev

--- sites/all/themes/reach/templates/views/views-view-unformatted--tools-and-resources--block.tpl.php
+++ sites/all/themes/reach/templates/views/views-view-unformatted--tools-and-resources--block.tpl.php
@@ -8,19 +8,19 @@ $main_imageurl = file_create_url($main_image);
 // $body = $row->field_field_lead_or_exp[0]['rendered']['#markup'];
 $body = $row->field_field_title[0]['rendered']['#markup'];
 $link = $row->field_field_know_link[0]['rendered']['#markup'];
-
+$location = $row->field_field_tags[0]['rendered']['#markup'];
 
 ?>
 
 
- <div class="col xl3 l4 m6 s6">
+ <div class="col xl4 l4 m6 s6">
                     <div class="card center">
                     	<a href="<?php print $link;?>" target="_blank">
                                   <img class="circle responsive-img" src="<?php print $main_imageurl;?>">
                                
                                 <div class="card-content">
                                     <h6 class="name"><?php print $title;?></h6>
-                                
+                                      <p><?php print $location;?></p>
                                      <p><?php print $body;?></p>
                                 </div>
                         </a>
